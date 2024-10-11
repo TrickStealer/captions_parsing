ad58b5cb9592eb3854f4bb107a8bf17275cecf6d
TrickStealer <pritykinigor@gmail.com>
2024-10-11 20:19:04 +0200

    Make fonctional for parsing from custom .kdenlive.srt file to .tsv

--- src/main.py
+++ src/main.py
@@ -1,19 +1,47 @@
-import pandas as pd
+# import pandas as pd
+import numpy as np
 
-first_filepath = "/mnt/files/coding/python/captions_parsing/caption_files/captions_basic_orbits_srb_eng.kdenlive.srt"
+firstFilepath = "/mnt/files/coding/python/captions_parsing/caption_files/captions_basic_orbits_srb_eng.kdenlive.srt"
+fullFilepath = "/mnt/files/coding/python/captions_parsing/caption_files/captions_basic_orbits_full.tsv"
 
-file = open(first_filepath, "r")
-content = file.read()
 
-modeTuple = (
-    'number',
-    'time',
-    'srb',
-    '-',
-    'eng',
-    'empty'
+with open(firstFilepath, 'r') as file:
+    content = file.readlines()
+
+columnsTuple = (
+    'number',   #0
+    'time',     #1
+    'srb',      #2
+    '-',        #3
+    'eng',      #4
+    'empty'     #5
 )
 
-modeNum = 0
-modeLen = len(modeTuple)
+contentLen = len(content)
+columnsLen = len(columnsTuple)
+rowsLen = contentLen//columnsLen + 1
+
+lineNum = 0
+for line in content:
+    content[lineNum] = line.strip('\n')
+    lineNum += 1
+
+content.append('')
+contentNP = np.array(content)
+captions = contentNP.reshape(rowsLen, columnsLen)
+# captionsT = captions.T
+
+a = captions[0]
+b = captions[1]
+
+resultFile = open(fullFilepath, 'w')
+
+for raw in range(rowsLen):
+    resultData = [
+        captions[raw][1], # time
+        captions[raw][2], # srb
+        captions[raw][4]  # eng
+    ]
+    resultLine = '\t'.join(resultData)
+    resultFile.write(resultLine + '\n')
 
